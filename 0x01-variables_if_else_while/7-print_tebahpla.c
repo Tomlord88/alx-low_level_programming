07ce6df909578e097a381527209fb11af6e3fc88
tomlord88 <tom.isokpeh@gmail.com>
2022-10-16 21:17:05 -0700

    7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - prints all single digit numbers of base 10 starting from 0,
  * followed by a new line, using putchar
@@ -7,12 +6,12 @@
  */
 int main(void)
 {
-	int n;
+        int n;
 
-	for (n = 48; n < 58; n++)
-	{
-		putchar(n);
-	}
-	putchar('\n');
-	return (0);
+        for (n = 48; n < 58; n++)
+        {
+                putchar(n);
+        }
+        putchar('\n');
+        return (0);
 }
